1d5089c5c4ff67283e43c779fb0cdf948b889095
brett.miller <brett.miller@sage.com>
2019-03-06 12:03:19 +0000

    Added module scaffolding

--- Public/Get-AzCachedAccessToken.ps1
+++ Public/Get-AzCachedAccessToken.ps1
@@ -16,11 +16,13 @@ function Get-AzCachedAccessToken {
     [cmdletbinding()]
     param()
 
-    $azureRmProfile = [Microsoft.Azure.Commands.Common.Authentication.Abstractions.AzureRmProfileProvider]::Instance.Profile
-
     $currentAzureContext = Get-AzContext
+
+    $azureRmProfile = [Microsoft.Azure.Commands.Common.Authentication.Abstractions.AzureRmProfileProvider]::Instance.Profile
     $profileClient = New-Object Microsoft.Azure.Commands.ResourceManager.Common.RMProfileClient($azureRmProfile)
+    
     Write-Verbose ("Tenant: {0}" -f  $currentAzureContext.Subscription.Name)
+    
     $token = $profileClient.AcquireAccessToken($currentAzureContext.Tenant.TenantId)
     $token.AccessToken
 }
